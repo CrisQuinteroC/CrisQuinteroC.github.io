cfade1964d2dc4ea6af8702bd4adf5182e404307
CrisQuinteroC <cristopher.qc@outlook.com>
2026-03-24 14:09:38 -0700

    Delete conexion.php

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "inventariomeca");
-$conexion->set_charset("utf8");
-
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-?>
